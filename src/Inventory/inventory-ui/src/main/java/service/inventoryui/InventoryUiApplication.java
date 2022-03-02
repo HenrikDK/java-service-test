fd1f63b32c34da811e04e040b083d0e8c0ade191
HenrikDK <commander.john.crichton@gmail.com>
2022-03-02 22:26:27 +0100

    add docker build of multi project spring boot application

---
+++ src/Inventory/inventory-ui/src/main/java/service/inventoryui/InventoryUiApplication.java
@@ -0,0 +1,13 @@
+package service.inventoryui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryUiApplication.class, args);
+    }
+
+}
---
+++ src/Inventory/inventory-ui/src/test/java/service/inventoryui/InventoryUiApplicationTests.java
@@ -0,0 +1,13 @@
+package service.inventoryui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
